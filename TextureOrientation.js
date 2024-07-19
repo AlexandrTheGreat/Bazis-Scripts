7f0aaf4c5739db5ce67dfcfde907215865cf90fa
AlexandrTheGreat <savka.alexandr@gmail.com>
2024-07-19 14:53:29 +0700

    Material's names extaction to js module

---
+++ TextureOrientation.js
@@ -0,0 +1,8 @@
+﻿const spotMaterials = ['ЛДСП 22 Белый 1850 ШПОН ТИС. \r293308)',];
+
+Model.forEachPanel(function(obj) {
+    if (spotMaterials.indexOf(obj.Materialname) > -1) obj.TextureOrientation = 2;
+    console.log(obj.TextureOrientation, obj.MaterialName);
+    obj.TextureOrientation = 0;
+
+})
